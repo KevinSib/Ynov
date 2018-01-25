eab4a1ea6882487b3ad803455fa930fef93d7587
victorDuchenet <victor.duchenet@ynov.com>
2018-01-25 17:12:45 +0100

    test ForUserService

--- YnovShop/YnovShopTest/UserServiceShould.cs
+++ YnovShop/YnovShopTest/UserServiceShould.cs
@@ -1,16 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using YnovShop.Business;
+using YnovShop.Data;
+using YnovShop.Provider;
 
 namespace YnovShopTest
 {
     [TestClass]
     public class UserServiceShould
     {
+        private Mock<IUserRepository> _userRepository;
+        private Mock<ISaltProvider> _saltProvider;
+        private Mock<IPasswordProvider> _passwordProvider;
+        private IUserService userService;
+        [TestInitialize]
+        public void Init()
+        {
+            //test crub bdd ? 
+            //injection dependance dans les tests ?
+            //tester si il envoye une exception
 
-                    //si password 
-            //test des champs null
-            //insertion en Bdd
+            _userRepository=new Mock<IUserRepository>(MockBehavior.Strict);
+            _saltProvider = new Mock<ISaltProvider>(MockBehavior.Strict);
+            _passwordProvider = new Mock<IPasswordProvider>(MockBehavior.Strict);
+            userService = new UserService(_userRepository.Object,_saltProvider.Object,_passwordProvider.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void returnExceptionIfPasswordIsEmpty()
+        {
+            userService.CreateUser("", "", "", "");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void returnExceptionIfPasswordIsNull()
+        {
+            userService.CreateUser("", "", "", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void returnExceptionIfEmailsNull()
+        {
+            userService.CreateUser("", "", null, "erer");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void returnExceptionIfEmailIsEmpty()
+        {
+            userService.CreateUser("", "", "", "rer");
+        }
     }
 }
