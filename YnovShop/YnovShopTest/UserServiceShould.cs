209e34bd199450cc0c8117707f49e4006f885e7a
Duchenet Victor <victorduchenet@gmail.com>
2018-01-27 01:14:45 +0100

    add Mocked test for createUser

--- YnovShop/YnovShopTest/UserServiceShould.cs
+++ YnovShop/YnovShopTest/UserServiceShould.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using YnovShop.Business;
 using YnovShop.Data;
+using YnovShop.Data.Entities;
 using YnovShop.Provider;
 
 namespace YnovShopTest
@@ -18,10 +19,13 @@ namespace YnovShopTest
         [TestInitialize]
         public void Init()
         {
-
+            
             _userRepository = new Mock<IUserRepository>(MockBehavior.Strict);
             _saltProvider = new Mock<ISaltProvider>(MockBehavior.Strict);
             _passwordProvider = new Mock<IPasswordProvider>(MockBehavior.Strict);
+            _saltProvider.Setup(sp => sp.GetSalt()).Returns(Convert.FromBase64String("aaaazzzzaaaazzzz"));
+            _passwordProvider.Setup(pp => pp.PasswordHash(It.IsAny<string>(), It.IsAny<Byte[]>()))
+                .Returns(Convert.FromBase64String("aaaazzzzaaaazzzz"));
             userService = new UserService(_userRepository.Object, _saltProvider.Object, _passwordProvider.Object);
         }
 
@@ -51,6 +55,14 @@ namespace YnovShopTest
         public void returnExceptionIfEmailIsEmpty()
         {
             userService.CreateUser("", "", "", "rer");
+        }
+
+        [TestMethod]
+        public void VerifyIfCerrateUSerIsSuccess()
+        {
+            _userRepository.Setup(us => us.Insert(It.IsAny<YUser>()));
+           userService.CreateUser("ezrzer","rezr","azer","rzerz");
+            _userRepository.Verify(s => s.Insert(It.IsAny<YUser>()));
         }
     }
 }
